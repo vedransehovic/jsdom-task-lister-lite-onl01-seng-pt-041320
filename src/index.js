d3a2a9b2dc90ff9475abc3afd8d0ca474486e2a3
vedransehovic <vedran.sehovic@gmail.com>
2020-10-23 17:16:11 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector("form");
+  form.addEventListener("submit", function () {
+    event.preventDefault();
+    const input = document.getElementById("new-task-description");
+    if (input.value !== "") {
+      const li = document.createElement("li");
+      li.innerText = input.value;
+      document.getElementById("tasks").append(li);
+      input.value = "";
+    }
+  });
 });
